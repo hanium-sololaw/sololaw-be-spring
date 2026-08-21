5b1b48658688607ff145ff8fc7bc84b47b3d5ad4
김나경 <1030n@naver.com>
2026-08-21 14:11:42 +0900

    ✨ Feat: presigned PUT 발급, 저장 용량 원자 증감 쿼리 추가
    S3Uploader에 generatePresignedPutUrl 추가. SubscriptionRepository에
    tryReserveStorage·releaseStorage 원자 UPDATE 추가해 read-then-write
    동시성 문제 없이 저장 용량 쿼터를 관리

--- src/main/java/com/hanium/sololaw/global/storage/S3Uploader.java
+++ src/main/java/com/hanium/sololaw/global/storage/S3Uploader.java
@@ -12,11 +12,14 @@ import com.hanium.sololaw.global.config.property.S3Properties;
 import lombok.RequiredArgsConstructor;
 import software.amazon.awssdk.core.sync.RequestBody;
 import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.model.DeleteObjectRequest;
 import software.amazon.awssdk.services.s3.model.GetObjectRequest;
 import software.amazon.awssdk.services.s3.model.PutObjectRequest;
 import software.amazon.awssdk.services.s3.presigner.S3Presigner;
 import software.amazon.awssdk.services.s3.presigner.model.GetObjectPresignRequest;
 import software.amazon.awssdk.services.s3.presigner.model.PresignedGetObjectRequest;
+import software.amazon.awssdk.services.s3.presigner.model.PresignedPutObjectRequest;
+import software.amazon.awssdk.services.s3.presigner.model.PutObjectPresignRequest;
 
 /**
  * S3 업로드·presigned URL 발급 공통 유틸. 도메인별로 key prefix(예: {@code documents/}, {@code evidence/})만 다르게
@@ -66,5 +69,40 @@ public class S3Uploader {
             .build();
     PresignedGetObjectRequest presigned = s3Presigner.presignGetObject(presignRequest);
     return presigned.url().toString();
+  }
+
+  /**
+   * 지정한 키로 업로드할 수 있는 presigned PUT URL을 발급합니다. 프론트가 이 URL로 스토리지에 직접 파일을 올린다.
+   *
+   * @param key 업로드될 S3 객체 키
+   * @param contentType MIME 타입
+   * @param expiry URL 유효 기간
+   * @return presigned PUT URL
+   */
+  public String generatePresignedPutUrl(String key, String contentType, Duration expiry) {
+    PutObjectRequest putObjectRequest =
+        PutObjectRequest.builder()
+            .bucket(s3Properties.getBucket())
+            .key(key)
+            .contentType(contentType)
+            .build();
+    PutObjectPresignRequest presignRequest =
+        PutObjectPresignRequest.builder()
+            .signatureDuration(expiry)
+            .putObjectRequest(putObjectRequest)
+            .build();
+    PresignedPutObjectRequest presigned = s3Presigner.presignPutObject(presignRequest);
+    return presigned.url().toString();
+  }
+
+  /**
+   * 지정한 키의 객체를 S3에서 삭제합니다.
+   *
+   * @param key 삭제할 S3 객체 키
+   */
+  public void deleteObject(String key) {
+    DeleteObjectRequest request =
+        DeleteObjectRequest.builder().bucket(s3Properties.getBucket()).key(key).build();
+    s3Client.deleteObject(request);
   }
 }
